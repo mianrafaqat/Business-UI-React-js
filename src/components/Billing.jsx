657c597d3d22a085d750e7fee109e6e98c49104b
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2023-02-19 22:07:25 +0500

    testimonial section

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -7,8 +7,8 @@ const Billing = () => {
     <section id='product' className={`${layout.sectionReverse}`}>
       <div className={`${layout.sectionImgReverse}`}>
         <img src={bill} alt="bill" className='w-[100%] h-[100%] relative z-[5] ' />
-        <div className=' absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white-gradient ' />
-        <div className=' absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink-gradient ' />
+        <div className=' absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient ' />
+        <div className=' absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink__gradient ' />
       </div>
 
       <div className={`${layout.sectionInfo}`}>
